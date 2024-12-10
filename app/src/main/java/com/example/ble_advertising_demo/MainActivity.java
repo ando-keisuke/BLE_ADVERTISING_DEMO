ffe335c90995fe7161688bba9e9600b543851c16
ANDO_KEISUKE <it242017@trident.ac.jp>
2024-12-10 23:35:19 +0900

    不要なimportを削除

--- app/src/main/java/com/example/ble_advertising_demo/MainActivity.java
+++ app/src/main/java/com/example/ble_advertising_demo/MainActivity.java
@@ -8,7 +8,6 @@ import android.bluetooth.le.AdvertiseData;
 import android.bluetooth.le.AdvertiseSettings;
 import android.bluetooth.le.BluetoothLeAdvertiser;
 import android.content.pm.PackageManager;
-import android.os.Build;
 import android.os.Bundle;
 import android.util.Log;
 import android.widget.Toast;
